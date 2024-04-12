75f698621f489f11371a2e6f637c476898339313
Mr.Nie <147066987+nyx-1037@users.noreply.github.com>
2024-04-12 18:40:35 +0800

    Add files via upload

---
+++ test20240412/test20240412/Main.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++>
+#include <windows.h>
+
+
+#define l1 262
+#define l2 294
+#define l3 330
+#define l4 349
+#define l5 392
+#define l6 440
+#define l7 493
+
+#define n1 532
+#define n2 588
+#define n3 660
+#define n4 698
+#define n5 784
+#define n6 880
+#define n7 988
+
+#define h1 1046
+#define h2 1175
+#define h3 1319
+#define h4 1397
+#define h5 1568
+#define h6 1760
+#define h7 1976
+
+#define A 1600*1.25 //кдед
+#define B 800*1.25  //а╫ед
+#define C 400*1.25  //р╩ед
+#define D 200*1.25  //╟Кед
+#define E 100*1.25  //1/4ед
+#define F 50*1.25   //1/8ед
+
+#define S(i) Sleep(i)
+#define wq( i , j ) Beep( i , j )
+
+int  main() {
+    wq(n3, D); wq(n4, D); wq(n3, D); wq(n4, D); wq(n5, C); wq(n3, D); wq(n4, D);
+
+    wq(n3, D); wq(n4, D); wq(n5, C); wq(n5, D); wq(n4, D); wq(n3, D); wq(n4, D);
+
+    wq(n3, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(l6, D);
+    wq(n3, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(l6, D);
+
+    wq(n1, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(l6, D); wq(n3, C); S(C);
+
+    wq(n3, D); wq(n4, D); wq(n3, D); wq(n4, D); wq(n5, C); wq(n3, D); wq(n4, D);
+
+    wq(n3, D); wq(n4, D); wq(n5, C); wq(n5, D); wq(n4, D); wq(n3, D); wq(n4, D);
+
+    wq(n3, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(l6, D);
+    wq(n3, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(n2, D);
+
+    wq(n1, E); wq(n1, D); wq(n1, D); wq(n1, E); wq(n3, D); wq(n3, C); S(C);
+
+    wq(n5, C); wq(n3, C); wq(n2, C); wq(n3, D); wq(n3, D);
+
+    wq(n2, D); wq(n1, D); wq(n2, D); wq(n3, B); wq(n3, D);
+
+    wq(n6, C); wq(n3, C); wq(n2, C); wq(n1, D); wq(n1, D);
+
+    wq(n1, D); wq(n2, D); wq(n3, D); wq(n2, C); S(C); S(D);
+
+    wq(n5, C); wq(n3, C); wq(n2, C); wq(n3, D); wq(n3, D);
+
+    wq(n2, D); wq(n1, D); wq(n2, D); wq(n3, B); wq(n3, D);
+
+    wq(n6, C); wq(n3, C); wq(n1, C); wq(n3, C);
+
+    wq(n1, C); S(C); S(C); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, 375); wq(l5, D); wq(n1, 375); wq(n1, E); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, 375); wq(l5, D); wq(n1, 375); wq(n3, E); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, D); wq(l6, E); wq(n5, D); wq(n1, 375); wq(n1, D); wq(n1, E); wq(n2, D);
+
+    wq(n1, 375); wq(n1, E); S(E); wq(n1, E); wq(l6, D); wq(n3, C); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, 375); wq(l5, D); wq(n1, 375); wq(n1, E); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, 375); wq(l5, D); wq(n3, 375); wq(n1, E); S(D); wq(n1, E); wq(l5, E);
+
+    wq(l6, 375); wq(l6, D); wq(l6, E); wq(n5, D); wq(n1, 375); wq(n1, D); wq(n1, E); wq(n2, D);
+
+    wq(n1, 375); wq(n1, D); wq(n3, 375); wq(n1, C); S(C);
+
+
+}
